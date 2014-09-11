2713705ee84b12707376b4738eccc216525900f4
Justin Hosman <jhoss05@gmail.com>
2014-09-11 10:48:06 -0600

    Did more exercises.

--- basic2.rb
+++ basic2.rb
@@ -1,4 +1,54 @@
-first_name = "Justin "
-last_name = "Hosman"
+# x = 0
+# 3.times do
+#   x += 1
+# end
+# puts x
 
-puts first_name + last_name
+
+
+# # puts "Please enter your first name:"
+# # first_name = gets.chomp
+# # puts "Please enter your last name:"
+# # last_name = gets.chomp
+
+# # 10.times do 
+# # 	puts "Hello there #{first_name + " " + last_name}. Are you prepared to practice?"
+# # end
+
+
+
+# # # puts "Please enter your age:"
+# # # age = gets.chomp.to_i
+
+# # # def age_in(num)
+# # # 	puts "In 10 years you will be #{num + 10}."
+# # # 	puts "In 20 years you will be #{num + 20}."
+# # # 	puts "In 30 years you will be #{num + 30}."
+# # # 	puts "In 40 years you will be #{num + 40}."
+# # # end
+
+# # # puts age_in(age)
+
+
+
+# # # puts 39.232 * 39.232
+# # # puts 3.4 * 3.4
+# # # puts 6.5 * 6.5
+
+
+
+# # # puts 3.43 * 34.56 * 39348.3
+
+
+
+# # # puts 5 * 4 * 3 * 2 * 1
+# # # puts 6 * 5 * 4 * 3 * 2 * 1
+# # # puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+# # # puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+
+
+# # # first_name = "Justin "
+# # # last_name = "Hosman"
+
+# # # puts first_name + last_name
